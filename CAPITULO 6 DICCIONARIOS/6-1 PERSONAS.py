8a6e5baba541eddc237110858e49326815d8038b
Héctor López Díaz <hector964@protonmail.com>
2026-05-05 19:28:08 +0200

    6-1 hecho

---
+++ CAPITULO 6 DICCIONARIOS/6-1 PERSONAS.py
@@ -0,0 +1,10 @@
+info_nombre = {
+    "nombre": "héctor",
+    "apellido": "lópez",
+    "edad": 24,
+    "ciudad": "castellón",
+}
+print(f"Nombre: {info_nombre["nombre"].title()}")
+print(f"Apellido: {info_nombre["apellido"].title()}")
+print(f"Edad: {info_nombre["edad"]}")
+print(f"Ciudad: {info_nombre["ciudad"].title()}")

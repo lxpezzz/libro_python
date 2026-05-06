b723e2fae5979d7bb744872b9217e4e3140d5cac
Héctor López Díaz <hector964@protonmail.com>
2026-05-06 12:21:27 +0200

    Ejercicio 6-2 y 6-3

---
+++ CAPITULO 6 DICCIONARIOS/6-2 NUMEROS FAVORITOS.py
@@ -0,0 +1,13 @@
+num_fav = {
+    "hector": 10,
+    "nerea": 7,
+    "alfredo": 6,
+    "ana": 5,
+    "irene": 3,
+}
+
+print(num_fav["hector"])
+print(num_fav["alfredo"])
+print(num_fav["ana"])
+print(num_fav["irene"])
+print(num_fav["nerea"])
---
+++ CAPITULO 6 DICCIONARIOS/6-3 GLOSARIO.py
@@ -0,0 +1,12 @@
+words_python = {
+    "lista": "Colección ordenada de elementos. Entre []",
+    "booleano": "Se usa para representar condiciones lógicas. True or False",
+    "diccionario": "Almacena datos mediante pares: clave : valor",
+    "tabulacion": "Espacio al inicio de una línea que Python usa para definir bloques de código.",
+    "sentencia if": "permite ejecutar código solo si una condición es verdadera.",
+}
+print(f"Lista:\n{words_python['lista']}\n")
+print(f"Booleano:\n{words_python['booleano']}\n")
+print(f"Diccionario:\n{words_python['diccionario']}\n")
+print(f"Tabulación:\n{words_python['tabulacion']}\n")
+print(f"Sentencia if:\n{words_python['sentencia if']}\n")

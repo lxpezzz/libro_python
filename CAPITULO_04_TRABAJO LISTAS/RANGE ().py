8eb7311a9d787ea44f0e6b981c8735769f4d215f
Héctor López Díaz <hector964@protonmail.com>
2026-05-15 20:55:51 +0200

    Ajusto teoria range()

--- CAPITULO_04_TRABAJO LISTAS/RANGE ().py
+++ CAPITULO_04_TRABAJO LISTAS/RANGE ().py
@@ -1,32 +1,39 @@
-#Señalizar el rango de valores
+# En range los 3 valores son range(Empieza, acaba, pasos)
+# EJ:
+# Empieza en 1 hasta el 27 con paso 3
+a = range(1, 27, 3)
+print(a)
+
+
+# Señalizar el rango de valores
 
 for value in range(1, 11):
     print(value)
 
 
-#como poner un listado de numeros
+# como poner un listado de numeros
 numeros = list(range(1, 6))
 print(numeros)
 
-#poner un listado de pares
+# poner un listado de pares
 pares = list(range(2, 11, 2))
 print(pares)
 
-###Numeros cuadreados 
+###Numeros cuadreados
 cuadrados = []
 for value in range(1, 11):
-    cuadrado = value ** 2
+    cuadrado = value**2
     cuadrados.append(cuadrado)
 print(cuadrados)
 
-#de forma más simple: 
+# de forma más simple:
 cuadrados = []
 for value in range(1, 11):
     cuadrados.append(value**2)
 print(cuadrados)
 
 ##COMO ENSEÑAR EL NUMERO MIN, MAX Y LA SUMA DE DIGITOS
-digits = [1,2,3,4,5,6,7,8,9,0]
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 print(min(digits))
 print(max(digits))
 print(sum(digits))
